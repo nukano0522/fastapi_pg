fa4bd1fa730ab357382637819c6556b5d9ac74a0
Shuhei.Nakano <sc100344@gintra.bc-glex.net>
2023-11-03 15:02:51 +0900

    Commit

--- sql_app/request.sh
+++ sql_app/request.sh
@@ -1,2 +1,2 @@
 # create an user
-curl -X POST -H "Content-Type: application/json" -d '{"email": "shunakano@benesse-style-care.co.jp", "password": "abc"}' 127.0.0.1:3000/users/
+curl -X POST -H "Content-Type: application/json" -d '{"email": "shunakano@test.com", "password": "abc"}' 127.0.0.1:3000/users/
